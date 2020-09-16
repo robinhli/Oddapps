880b7484011285cf15e79fdfe4d90b4e649556e0
lihaibin <lihaibin@dashu-tech.com>
2020-09-16 11:06:00 +0800

    initial 13.0

--- defects_location/__manifest__.py
+++ defects_location/__manifest__.py
@@ -18,7 +18,7 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Uncategorized',
-    "version": "12.0.1.0.1",
+    "version": "13.0.1.0.1",
 
     # any module necessary for this one to work correctly
     'depends': ['stock'],
--- defects_location/models/stock_location.py
+++ defects_location/models/stock_location.py
@@ -11,6 +11,7 @@ _logger = logging.getLogger(__name__)
 
 class StockLocation(models.Model):
     _inherit = "stock.location"
+
     defects = fields.Boolean("Defects Products", default = False,
             help="True for not conforming products(defects), False for default. For internal usage location only")
     # to_be_checked = fields.Boolean("To Be Checked", help = "To Be Checked Location. Will not be in valued stock")
