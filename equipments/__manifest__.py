636c71574157b6482096a6c0e1c5b03ed60aabcc
lihaibin <lihaibin@dashu-tech.com>
2020-10-13 22:51:18 +0800

    equipment 14.0

--- equipments/__manifest__.py
+++ equipments/__manifest__.py
@@ -18,8 +18,8 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Manufacturing',
-    'version': '13.0.1',
+    'category': 'Manufacturing/Manufacturing',
+    'version': '14.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['maintenance', 'mrp'],
