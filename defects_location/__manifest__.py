728e64f1b710d1bc9e8f65f11f575beb0a5e0323
lihaibin <lihaibin@dashu-tech.com>
2020-10-13 22:52:02 +0800

    equipment 14.0

--- defects_location/__manifest__.py
+++ defects_location/__manifest__.py
@@ -17,7 +17,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Operations/Inventory',
+    'category': 'Inventory/Inventory',
     "version": "14.0.1.0.1",
 
     # any module necessary for this one to work correctly
