ca6e54cd233369b8bac99914b989cdda06bc5ad4
lihaibin <lihaibin@dashu-tech.com>
2021-10-22 14:32:09 +0800

    equipments

--- equipments/__manifest__.py
+++ equipments/__manifest__.py
@@ -19,7 +19,7 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Manufacturing/Manufacturing',
-    'version': '14.0.1',
+    "version": "15.0.1.0.1",
 
     # any module necessary for this one to work correctly
     'depends': ['maintenance', 'mrp'],
