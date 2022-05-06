ea83635f7e2bf5673bad3998aea4640037cb3077
Li <lihaibin@dashu-tech.com>
2022-05-06 14:31:44 +0800

    fix

--- ds_purchase_track/models/purchase.py
+++ ds_purchase_track/models/purchase.py
@@ -2,23 +2,19 @@
 
 from odoo import models, fields, api
 
+
 class Purchaes(models.Model):
     _inherit = 'purchase.order'
 
-    order_line = fields.One2many('purchase.order.line', 'order_id', string='Order Lines', tracking=True,
-                                 states={'cancel': [('readonly', True)], 'done': [('readonly', True)]}, copy=True)
+    order_line = fields.One2many(tracking=True)
 
-    def write(self, vals):
-        res = super(Purchaes, self).write(vals)
-
-        return res
 
 class PurchaseLine(models.Model):
     _inherit = 'purchase.order.line'
 
-    product_qty = fields.Float(string='Quantity', digits='Product Unit of Measure', required=True, tracking=True)
-    date_planned = fields.Datetime(string='Scheduled Date', index=True, tracking=True)
-    taxes_id = fields.Many2many('account.tax', string='Taxes', tracking=True, domain=['|', ('active', '=', False), ('active', '=', True)])
-    product_uom = fields.Many2one('uom.uom', string='Unit of Measure', tracking=True, domain="[('category_id', '=', product_uom_category_id)]")
-    product_id = fields.Many2one('product.product', string='Product', domain=[('purchase_ok', '=', True)], change_default=True, tracking=True)
-    price_unit = fields.Float(string='Unit Price', required=True, digits='Product Price', tracking=True)
+    product_qty = fields.Float(tracking=True)
+    date_planned = fields.Datetime(tracking=True)
+    taxes_id = fields.Many2many(tracking=True)
+    product_uom = fields.Many2one(tracking=True)
+    product_id = fields.Many2one(tracking=True)
+    price_unit = fields.Float(tracking=True)
